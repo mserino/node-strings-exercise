e8a8162d5ea8f9078f58d5233f12e86c0c64c460
Maggie Serino <m.serino@hotmail.it>
2018-05-10 12:48:46 +0200

    it returns each single pair if there is more than one

--- spec/stringsSpec.js
+++ spec/stringsSpec.js
@@ -3,7 +3,7 @@ const app = require('../scripts/app');
 describe('Given an array of strings', () => {
   it('returns the pair if they have the same number of letters', () => {
     const strings = ['dog', 'god', 'arrow', 'balloon'];
-    const expected = 'dog,god';
+    const expected = ['dog,god'];
 
     expect(app.manipulateStrings(strings)).toEqual(expected);
   });
@@ -20,5 +20,10 @@ describe('Given an array of strings', () => {
     const expected = '';
 
     expect(app.manipulateStrings(strings)).toEqual(expected);
+  });
+
+  it('returns each single pair if there is more than one', () => {
+    const strings = ['dog', 'god', 'overcast', 'overacts', 'balloon', 'rainbow'];
+    const expected = ['dog,god', 'overcast,overacts']
   });
 });
